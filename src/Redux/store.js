99739d4d0b72a82fac9d139ab9a5d348a4ed6bd0
Mani181297 <manideep.r@ibhubs.co>
2020-10-10 14:36:04 +0530

    applymoddleware implemented

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
+import logger from 'redux-logger'
 import { cakeReducer } from './Cake/CakeReducer'
 import rootReducer from './RootReducer'
 
 // const Store=createStore(cakeReducer)
-const Store=createStore(rootReducer)
+// const Store=createStore(rootReducer)
+const Store=createStore(rootReducer,applyMiddleware(logger))
 
 export default Store
